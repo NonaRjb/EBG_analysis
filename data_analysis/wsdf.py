816d754c934d5819399e691a47aa5058791948a6
Nona Rajabi <rajabi.nona@gmail.com>
2024-04-18 08:41:16 +0200

    Start to Implement WSDF Method

---
+++ data_analysis/wsdf.py
@@ -0,0 +1,8 @@
+cluster_data_path = '/proj/berzelius-2023-338/users/x_nonra/data/Smell/'
+cluster_save_path = '/proj/berzelius-2023-338/users/x_nonra/data/Smell/'
+local_data_path = "/Volumes/T5 EVO/Smell/"
+local_save_path = "/Volumes/T5 EVO/Smell/"
+
+
+if __name__ == "__main__":
+    pass
